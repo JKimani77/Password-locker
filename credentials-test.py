948d4bff8150453bb287fa02c08c461683e30b0c
JKimani77 <jkkimani77@gmail.com>
2019-06-17 21:52:10 +0300

    second unit test done and resolved indentation errors

--- credentials-test.py
+++ credentials-test.py
@@ -14,3 +14,9 @@ class TestCredentials(unittest.TestCase):
         '''
 
         self.new_credentials = Credentials("Google", "3344")
+
+    def tearDown(self):
+        '''
+         tear down method that does clean up after each test case has run.
+        '''
+        Credentials.credentials_list = []
