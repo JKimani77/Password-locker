063e7e32c2a449ca75e4f391cff018989157e135
JKimani77 <jkkimani77@gmail.com>
2019-06-17 21:53:05 +0300

    third unit test done

--- credentials-test.py
+++ credentials-test.py
@@ -20,3 +20,12 @@ class TestCredentials(unittest.TestCase):
          tear down method that does clean up after each test case has run.
         '''
         Credentials.credentials_list = []
+
+    def test_credentials_instance(self):
+        '''
+        method to test if new credentials have been instanciated properly
+        '''
+
+        self.assertEqual(self.new_credentials.account_name, "Google")
+        self.assertEqual(self.new_credentials.account_password, "3344")
+    
